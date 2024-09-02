37ac3d948959355ae221f9cb0f6d71af066289bf
Bart Thate <rssbotd@gmail.com>
2024-09-02 16:34:00 +0200

    update package docstring

--- rssbot/__init__.py
+++ rssbot/__init__.py
@@ -2,5 +2,85 @@
 # pylint: disable=C,I,R
 
 
-"NIXT"
+"""24/7 Feed Fetcher"
 
+
+SYNOPSIS
+
+    rssbot <cmd> [key=val] [key==val]
+
+
+DESCRIPTION
+
+    RSSBOT is a python3 bot able to display rss feeds in your channel.
+
+    RSSBOT comes with a cli to configure and a daemon to run in the
+    background, hooking the daemon in systemd brings a 24/7 available
+    rssbot in your channel.
+
+
+INSTALL
+
+    $ pipx install rssbot
+    $ pipx ensurepath
+
+    <new terminal>
+
+    $ rssbot srv > rssbot.service
+    $ sudo mv rssbot.service /etc/systemd/system/
+    $ sudo systemctl enable rssbot --now
+    $ rssbot rss <url>
+
+    joins #rssbot on localhost
+    
+
+COMMANDS
+
+    cfg - irc configuration
+    cmd - commands
+    dpl - sets display items
+    exp - export opml (stdout)
+    imp - import opml
+    mre - displays cached output
+    pwd - sasl nickserv name/pass
+    rem - removes a rss feed
+    res - restore deleted objects
+    rss - add a feed
+    syn - sync rss feeds
+
+
+CONFIGURATION
+
+    irc
+
+    $ rssbot cfg server=<server>
+    $ rssbot cfg channel=<channel>
+    $ rssbot cfg nick=<nick>
+
+    sasl
+ 
+    $ rssbot pwd <nsvnick> <nspass>
+    $ rssbot cfg password=<frompwd>
+
+    rss
+
+    $ rssbot rss <url>
+    $ rssbot dpl <url> <item1,item2>
+    $ rssbot rem <url>
+    $ rssbot res <url>
+    $ rssbot nme <url> <name>
+
+    opml
+
+    $ rssbot exp
+    $ rssbot imp <filename>
+
+
+COPYRIGHT
+
+    RSSBOT is Public Domain.
+
+"""
+
+
+__author__ = "Bart Thate <rssbotd@gmail.com>"
